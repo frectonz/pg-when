3d1acade095bc55e3c77e4dd4d6431affa1764d6
frectonz <fraol0912@gmail.com>
2025-07-07 11:11:57 +0300

    feat: add more functions to return the instant in different ways

--- src/when_is.rs
+++ src/when_is.rs
@@ -5,8 +5,40 @@ use pgrx::prelude::*;
 use crate::WhenInput;
 
 #[pg_extern(strict, immutable, parallel_safe)]
-fn when_is(input: &str) -> i64 {
+fn when_is(input: &str) -> pgrx::datum::TimestampWithTimeZone {
+    let (_, input) = WhenInput::parse(input).unwrap();
+    let zoned = input.to_timestamp().unwrap();
+    let zoned = zoned.with_time_zone(jiff::tz::TimeZone::UTC);
+
+    pgrx::datum::TimestampWithTimeZone::with_timezone(
+        zoned.year() as i32,
+        zoned.month() as u8,
+        zoned.day() as u8,
+        zoned.hour() as u8,
+        zoned.minute() as u8,
+        zoned.second() as f64,
+        "UTC",
+    )
+    .unwrap()
+}
+
+#[pg_extern(strict, immutable, parallel_safe)]
+fn seconds_at(input: &str) -> i64 {
     let (_, input) = WhenInput::parse(input).unwrap();
     let zoned = input.to_timestamp().unwrap();
     zoned.timestamp().as_second()
+}
+
+#[pg_extern(strict, immutable, parallel_safe)]
+fn millis_at(input: &str) -> i64 {
+    let (_, input) = WhenInput::parse(input).unwrap();
+    let zoned = input.to_timestamp().unwrap();
+    zoned.timestamp().as_millisecond()
+}
+
+#[pg_extern(strict, immutable, parallel_safe)]
+fn micros_at(input: &str) -> i64 {
+    let (_, input) = WhenInput::parse(input).unwrap();
+    let zoned = input.to_timestamp().unwrap();
+    zoned.timestamp().as_microsecond()
 }
