6bbdac915f645395586d131596e4dcfea16dc96b
frectonz <fraol0912@gmail.com>
2025-07-09 21:52:40 +0300

    chore: 0.1.4

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
         pkgs:
         let
           pname = "pg-when";
-          version = "0.1.3";
+          version = "0.1.4";
 
           buildPgliteFusionImage =
             {
--- nix/build.nix
+++ nix/build.nix
@@ -43,7 +43,7 @@ let
 in
 pkgs.rustPlatform.buildRustPackage {
   pname = "pg-when";
-  version = "0.1.3";
+  version = "0.1.4";
 
   src = pkgs.lib.cleanSourceWith {
     src = pkgs.lib.cleanSource ../.;
